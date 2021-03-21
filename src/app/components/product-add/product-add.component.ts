7ee19cc6e64a27c007258ea30ae09808313ae758
Mustafa Faruk Esen <mustafarukesen@gmail.com>
2021-03-21 14:18:57 +0300

    20 Mart

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -34,7 +34,6 @@ export class ProductAddComponent implements OnInit {
     if(this.productAddForm.valid){
       let productModel = Object.assign({}, this.productAddForm.value) 
       this.productService.add(productModel).subscribe(response => {
-        console.log(response)
         this.toastrService.success(response.message, "Başarılı!")
       },responseError=>{
         if(responseError.error.ValidationErrors.length>0){
